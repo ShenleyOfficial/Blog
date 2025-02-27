465def823b2de8e9118e8e5d06f05d7121e3d090
Shenley <582783985@qq.com>
2025-02-27 15:48:06 +0800

    Refactor Footer component from Astro to JSX

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+export default function Footer() {
+  return (
+      <footer className="p-6 text-xs bg-background-secondary dark:bg-background-secondary-dark text-text-secondary dark:text-text-secondary-dark">
+          <div className="flex justify-between">
+              <p>Copyright © 2025 Shenley All Rights Reserved.</p>
+              <p>Powered By Astro · Designed By Shenley.</p>
+          </div>
+    </footer>
+  );
+}
